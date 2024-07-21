6a378f2d7594f6c86022699b129e0d550319e120
bob <bobmcwishon@gmail.com>
2024-07-21 15:31:57 +0200

    added a insertAt method

--- linkedList.js
+++ linkedList.js
@@ -1,8 +1,8 @@
- class LinkedList {
+class LinkedList {
   constructor() {
     this.head = null;
   }
-   append(value) {
+  append(value) {
     if (this.head === null) {
       this.head = new Node(value);
     } else {
@@ -14,102 +14,142 @@
           goToNextNode(node.nextNode);
         }
       }
-    goToNextNode(this.head)
+      goToNextNode(this.head);
     }
   }
-  prepend(value){
-    if (this.head === null) {this.head = new Node(value)}
-    else{
-    const temp = this.head
-    this.head = new Node(value)
-    this.head.nextNode = temp
+  prepend(value) {
+    if (this.head === null) {
+      this.head = new Node(value);
+    } else {
+      const temp = this.head;
+      this.head = new Node(value);
+      this.head.nextNode = temp;
     }
-
   }
-   toString(){
-    if (this.head === null){
-        console.log("list is empty")
-        return
+  toString() {
+    if (this.head === null) {
+      console.log("list is empty");
+      return;
     }
-   function print(node, stringrep = ""){
-        if (node.nextNode === null){
-            stringrep = stringrep + ' -> ' + node.value
-            console.log(stringrep)
+    function print(node, stringrep = "") {
+      if (node.nextNode === null) {
+        stringrep = stringrep + " -> " + node.value;
+        console.log(stringrep);
+        return;
+      } else {
+        if (stringrep === "") {
+          stringrep = node.value;
+        } else {
+          stringrep = stringrep + " -> " + node.value;
+        }
+        print(node.nextNode, stringrep);
+      }
+    }
+    print(this.head);
+  }
+  getSize() {
+    if (this.head === null) {
+      return 0;
+    }
+    function findSize(node, current = 0) {
+      if (node.nextNode === null) {
+        current += 1;
+        return current;
+      }
+      current += 1;
+      return findSize(node.nextNode, current);
+    }
+    return findSize(this.head);
+  }
+  getHead() {
+    return this.head.value;
+  }
+  getTail() {
+    function findLast(node) {
+      if (node.nextNode === null) {
+        return node.value;
+      }
+      return findLast(node.nextNode);
+    }
+    return findLast(this.head);
+  }
+  getAtIndex(index) {
+    if (index > this.getSize()) {
+      throw new Error("index out of range");
+    }
+    function find(node, current = 0) {
+      if (current === index) {
+        return node.value;
+      }
+      current += 1;
+      return find(node.nextNode, current);
+    }
+    return find(this.head);
+  }
+  pop() {
+    if (this.head.nextNode === null) {
+      this.head = null;
+    }
+    function findSecondToLast(node, previous = null) {
+      if (node.nextNode === null) {
+        return previous;
+      }
+      return findSecondToLast(node.nextNode, node);
+    }
+    const newLast = findSecondToLast(this.head);
+    newLast.nextNode = null;
+  }
+  contains(value) {
+    if (this.head === null) {
+      return false;
+    }
+    function isValue(node) {
+      if (node.value === value) {
+        return true;
+      } else if (node.nextNode === null) {
+        return false;
+      } else {
+        return isValue(node.nextNode);
+      }
+    }
+    return isValue(this.head);
+  }
+  find(value) {
+    if (this.head === null) {
+      return null;
+    }
+    function helper(node, index = 0) {
+      if (node.value === value) {
+        return index;
+      }
+      if (node.nextNode === null) {
+        return null;
+      }
+      return helper(node.nextNode, index + 1);
+    }
+    return helper(this.head);
+  }
+  insertAt(index, value) {
+    if (index === 0) {
+      this.prepend(value);
+      return;
+    }
+    //arrow functions dont have their own 'this' which allows us access to the object inside the function
+    const goToIndex = (node, previous = null, current = 0) => {
+      if (current === index) {
+        const inserted = new Node(value);
+        inserted.nextNode = node;
+        previous.nextNode = inserted;
+      } else if (node.nextNode === null) {
+            this.append(value)
             return
         }
-        else{
-            if (stringrep === "") {stringrep = node.value}
-            else{
-            stringrep = stringrep + ' -> ' + node.value
-            }
-            print(node.nextNode, stringrep)
-        }
-   } 
-   print(this.head)
-  }
-  getSize(){
-    if (this.head === null){return 0}
-   function findSize(node, current=0){
-    if(node.nextNode === null){
-        current += 1
-        return current
-    } 
-    current += 1
-    return findSize(node.nextNode,current)
-   } 
-   return findSize(this.head)
-  }
-  getHead(){
-    return this.head.value
-  }
-  getTail(){
-    function findLast(node){
-        if (node.nextNode === null){return node.value}
-        return findLast(node.nextNode)
+        goToIndex(node.nextNode, node, current + 1);
+      }
+      goToIndex(this.head)
     }
-    return findLast(this.head)
   }
-  getAtIndex(index){
-    if (index > this.getSize()){throw new Error("index out of range")}
-    function find(node, current=0){
-        if (current === index){
-            return node.value
-        }
-        current+= 1
-        return find(node.nextNode,current)
-    }
-    return find(this.head)
-  }
-  pop(){
-    if(this.head.nextNode === null){this.head = null}
-    function findSecondToLast(node,previous=null){
-        if (node.nextNode === null){return previous}
-        return findSecondToLast(node.nextNode,node)
-    }
-    const newLast = findSecondToLast(this.head)
-    newLast.nextNode = null
-  }
-  contains(value){
-    if(this.head===null){return false}
-    function isValue(node){
-        if(node.value === value){return true}
-        else if(node.nextNode === null){return false}
-        else{
-        return isValue(node.nextNode)
-        }
-    }
-    return isValue(this.head)
-  }
-  find(value){
-    if (this.head===null){return null}
-    function helper(node,index=0){
-        if(node.value === value){return index}
-        if(node.nextNode === null){return null}
-        return helper(node.nextNode,index+1)
-    }
-    return helper(this.head)
-  }
-}
+
 class Node {
   constructor(value) {
     this.value = value;
@@ -117,21 +157,20 @@ class Node {
   }
 }
 
-
-
-myList = new LinkedList
-myList.append('cat')
-myList.append('dog')
-myList.append('cow')
-myList.prepend('horse')
-myList.toString()
-console.log(myList.getHead())
-console.log(myList.getTail())
-console.log(myList.getSize())
-console.log(myList.getAtIndex(2))
-myList.pop()
-myList.toString()
-console.log(myList.contains('dog'))
-console.log(myList.contains('siema'))
-console.log(myList.find('dog'))
-
+myList = new LinkedList();
+myList.append("cat");
+myList.append("dog");
+myList.append("cow");
+myList.prepend("horse");
+myList.toString();
+console.log(myList.getHead());
+console.log(myList.getTail());
+console.log(myList.getSize());
+console.log(myList.getAtIndex(2));
+myList.pop();
+myList.toString();
+console.log(myList.contains("dog"));
+console.log(myList.contains("siema"));
+console.log(myList.find("dog"));
+myList.insertAt(100, "turtle");
+myList.toString();

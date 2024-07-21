2c4864b39600f72d8e362d8c36c0ad5689d36f93
bob <bobmcwishon@gmail.com>
2024-07-21 15:54:13 +0200

    moved testing fuctionality to main.js

--- linkedList.js
+++ linkedList.js
@@ -1,4 +1,4 @@
-class LinkedList {
+module.exports = class LinkedList {
   constructor() {
     this.head = null;
   }
@@ -175,22 +175,4 @@ class Node {
   }
 }
 
-myList = new LinkedList();
-myList.append("cat");
-myList.append("dog");
-myList.append("cow");
-myList.prepend("horse");
-myList.toString();
-console.log(myList.getHead());
-console.log(myList.getTail());
-console.log(myList.getSize());
-console.log(myList.getAtIndex(2));
-myList.pop();
-myList.toString();
-console.log(myList.contains("dog"));
-console.log(myList.contains("siema"));
-console.log(myList.find("dog"));
-myList.insertAt(1, "turtle");
-myList.toString();
-myList.removeAt(3)
-myList.toString()
+
--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-import './linkedList'
+const LinkedList = require('./linkedList.js')
+myList = new LinkedList();
+myList.append("cat");
+myList.append("dog");
+myList.append("cow");
+myList.prepend("horse");
+myList.toString();
+console.log(myList.getHead());
+console.log(myList.getTail());
+console.log(myList.getSize());
+console.log(myList.getAtIndex(2));
+myList.pop();
+myList.toString();
+console.log(myList.contains("dog"));
+console.log(myList.contains("siema"));
+console.log(myList.find("dog"));
+myList.insertAt(1, "turtle");
+myList.toString();
+myList.removeAt(3)
+myList.toString()

e72b28a8627a783eac7e9ba0a7c2a78fc2842478
bob <bobmcwishon@gmail.com>
2024-07-27 16:22:28 +0200

    added an insert method

--- binaryTreeTraversal.js
+++ binaryTreeTraversal.js
@@ -1,36 +1,61 @@
-class Node{
-    constructor(value){
-        this.value = value
-        this.leftChild = null
-        this.rightChild = null
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.leftChild = null;
+    this.rightChild = null;
+  }
 }
-class BinaryTree{
-    constructor(values){
-        values.sort()
-        this.root = this.buildTree(values,0,values.length -1)
+class BinaryTree {
+  constructor(values) {
+    values.sort();
+    this.root = this.buildTree(values, 0, values.length - 1);
+  }
+  buildTree(values, start, end) {
+    if (start > end) {
+      return null;
     }
-    buildTree(values,start,end){
-        if(start>end){return null}
-        const mid = Math.floor((start+end)/2)
-        const root = new Node(values[mid])
-        root.leftChild = this.buildTree(values,start,mid-1)
-        root.rightChild = this.buildTree(values,mid+1,end)
-        return root
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(values[mid]);
+    root.leftChild = this.buildTree(values, start, mid - 1);
+    root.rightChild = this.buildTree(values, mid + 1, end);
+    return root;
+  }
+  insert(value) {
+    function findWhereToInsert(node) {
+      if (node.value > value) {
+        if (node.leftChild === null) {
+          node.leftChild = new Node(value);
+          return;
+        } else {
+          findWhereToInsert(node.leftChild);
+        }
+      }
+      if (node.value < value) {
+        if (node.rightChild === null) {
+          node.rightChild = new Node(value);
+          return;
+        } else {
+          findWhereToInsert(node.rightChild);
+        }
+      }
     }
+    findWhereToInsert(this.root);
+  }
 }
-const myTree = new BinaryTree([1,3,5,4,7,8,9])
+const myTree = new BinaryTree([45, 13, 5, 20, 7, 19, 9]);
 
 const prettyPrint = (node, prefix = "", isLeft = true) => {
-    if (node === null) {
-      return;
-    }
-    if (node.rightChild !== null) {
-      prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
-    if (node.leftChild !== null) {
-      prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  };
-  prettyPrint(myTree.root)
+  if (node === null) {
+    return;
+  }
+  if (node.rightChild !== null) {
+    prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  }
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+  if (node.leftChild !== null) {
+    prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  }
+};
+myTree.insert(0);
+myTree.insert(6);
+prettyPrint(myTree.root);

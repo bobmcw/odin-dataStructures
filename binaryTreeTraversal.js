a89266a678bbeaa5eafa75b0efc5105087745833
bob <bobmcwishon@gmail.com>
2024-07-27 15:55:48 +0200

    created a buildTree method in the binary tree class

--- binaryTreeTraversal.js
+++ binaryTreeTraversal.js
@@ -7,6 +7,29 @@ class Node{
 }
 class BinaryTree{
     constructor(values){
-        this.root = this.buildTree(values)
+        this.root = this.buildTree(values,0,values.length -1)
+    }
+    buildTree(values,start,end){
+        if(start>end){return null}
+        const mid = Math.floor((start+end)/2)
+        const root = new Node(values[mid])
+        root.leftChild = this.buildTree(values,start,mid-1)
+        root.rightChild = this.buildTree(values,mid+1,end)
+        return root
     }
 }
+const myTree = new BinaryTree([1,2,3,4,5,6,7,8,9])
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.rightChild !== null) {
+      prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.leftChild !== null) {
+      prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  };
+  prettyPrint(myTree.root)

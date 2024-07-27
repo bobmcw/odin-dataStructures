81164b28f0f5197f494ae23eac2d41a87e7f10a6
bob <bobmcwishon@gmail.com>
2024-07-27 18:17:22 +0200

    covered 2 out of 3 cases for deleting items from a binary search tree

--- binaryTreeTraversal.js
+++ binaryTreeTraversal.js
@@ -44,6 +44,63 @@ class BinaryTree {
     }
     findWhereToInsert(this.root);
   }
+  deleteItem(value){
+    function del(node,previousNode=null){
+        if(node === null){return}
+        if(value === node.value){
+            //case 1: no children
+            if(node.leftChild === null && node.rightChild === null){
+                if(previousNode.leftChild === node){
+                    previousNode.leftChild = null
+                }
+                else{
+                    previousNode.rightChild = null
+                }
+                return
+            }
+            //case2: one child
+            else if(node.leftChild !== null && node.rightChild === null){
+                node.value = node.leftChild.value
+                if (node.leftChild.rightChild !== null){
+                node.rightChild = node.leftChild.rightChild
+                }
+                else{
+                    node.rightChild = null
+                }
+                if(node.leftChild.leftChild !== null){    
+                node.leftChild = node.leftChild.leftChild
+                }
+                else{
+                    node.leftChild = null
+                }
+                return
+            }
+            else if(node.leftChild === null && node.rightChild !== null){
+                node.value = node.rightChild.value
+                if (node.rightChild.leftChild !== null){
+                node.leftChild = node.rightChild.leftChild
+                }
+                else{
+                    node.rightChild = null
+                }
+                if(node.rightChild.rightChild !== null){    
+                node.rightChild = node.rightChild.rightChild
+                }
+                else{
+                    node.rightChild = null
+                }
+                return
+            }
+        }
+         else{
+             del(node.leftChild,node)
+             del(node.rightChild,node)
+         }
+        
+
+    }
+    del(this.root)
+  }
 }
 const myTree = new BinaryTree([45, 13, 5, 20, 7, 19, 9]);
 
@@ -61,4 +118,6 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
 };
 myTree.insert(0);
 myTree.insert(6);
+prettyPrint(myTree.root);
+console.log('---------------------------')
 prettyPrint(myTree.root);
